d25e67dac5f9bae0114d17afaf9b03698ffe09c3
Danila <danila.pekhtserau@gmail.com>
2023-04-26 16:37:55 +0300

    modify models, fill seeds

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,4 @@
 class Bill < ApplicationRecord
+  belongs_to :user
+  belongs_to :room
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,65 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+users = User.create([
+                      {
+                        email: "myemail@gmail.com",
+                        password: 'valid_password',
+                        admin: false
+                      },
+                      {
+                        email: "UwUmail@gmail.com",
+                        password: "Dox72r465de",
+                        admin: false
+                      },
+                      {
+                        email: "mail@mail.com",
+                        password: "c3749ft83cft",
+                        admin: true
+                      }
+                    ])
+
+rooms = Room.create!([
+                       {
+                         places: 1,
+                         room_class: "normal",
+                         price: 10
+                       },
+                       {
+                         places: 2,
+                         room_class: "normal",
+                         price: 18
+                       },
+                       {
+                         places: 3,
+                         room_class: "lux",
+                         price: 100
+                       },
+                       {
+                         places: 2,
+                         room_class: "business",
+                         price: 50
+                       },
+                     ])
+
+Request.create!([
+                             {
+                               places: 2,
+                               room_class: "lux",
+                               time_of_stay: 7,
+                               user: users.first
+                             },
+                             {
+                               places: 1,
+                               room_class: "normal",
+                               time_of_stay: 7,
+                               user: users[2]
+                             },
+                           ])
+Bill.create!([
+                       {
+                         cost: 126,
+                         room: rooms[2],
+                         user: users.first
+                       }
+                     ])
