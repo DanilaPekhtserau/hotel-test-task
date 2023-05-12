4c02ce7272e1a01f5043affe975c7690979c5ec5
Danila <danila.pekhtserau@gmail.com>
2023-05-12 17:07:11 +0300

    add sorting direction for queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -12,15 +12,16 @@ module Types
     field :requests, [RequestType], null: false do
       argument :places, Integer, required: false
       argument :order_by, String, required: false
+      argument :sorting_direction, String, required: false
     end
 
-    def requests(places: nil, order_by: nil)
+    def requests(places: nil, order_by: nil, sorting_direction: '')
       user = context[:current_user]
       raise GraphQL::ExecutionError, 'User does not exist' unless !user.nil? && User.exists?(user.id)
 
       requests = Pundit.policy_scope(user, Request)
       requests = requests.where(places:) if places.present?
-      requests = requests.order(order_by) if order_by.present?
+      requests = requests.order(order_by + " #{sorting_direction}") if order_by.present?
 
       requests
     end
@@ -28,15 +29,16 @@ module Types
     field :bills, [BillType], null: false do
       argument :client_id, ID, required: false
       argument :order_by, String, required: false
+      argument :sorting_direction, String, required: false
     end
 
-    def bills(client_id: nil, order_by: nil)
+    def bills(client_id: nil, order_by: nil, sorting_direction: '')
       user = context[:current_user]
       raise GraphQL::ExecutionError, 'User does not exist' unless !user.nil? && User.exists?(user.id)
 
       bills = Pundit.policy_scope(user, Bill)
       bills = bills.where(user_id: client_id) if client_id.present?
-      bills = bills.order(order_by) if order_by.present?
+      bills = bills.order(order_by + " #{sorting_direction}") if order_by.present?
 
       bills
     end
